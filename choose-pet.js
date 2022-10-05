15b9027d92b676752fa2069aa4736de67eea7679
Barky213 <97125482+Barky213@users.noreply.github.com>
2022-10-05 16:34:42 +0300

    Update results - make cleaner- finish mobile style

--- choose-pet.js
+++ choose-pet.js
@@ -49,23 +49,26 @@ const getBreeds = async () => {
   if (checkedResults.length > 0) {
     displayResults();
   } else {
-    const errorMessage = document.createElement('h4');
-    errorMessage.innerText ='Sorry! There are no cats with that combination. Please try again!';
+    const errorMessage = document.createElement("h4");
+    errorMessage.innerText =
+      "Sorry! There are no cats with that combination. Please try again!";
     addResult.appendChild(errorMessage);
 
-    const errorImage = document.createElement('img');
-    errorImage.src = 'https://static5.tgstat.ru/channels/_0/c0/c05c08dd5b20108f2990b095f1fbe328.jpg';
+    const errorImage = document.createElement("img");
+    errorImage.src =
+      "https://static5.tgstat.ru/channels/_0/c0/c05c08dd5b20108f2990b095f1fbe328.jpg";
     addResult.appendChild(errorImage);
   }
 };
 
 const displayResults = () => {
-  const resultsIntro = document.createElement('h3');
+  const resultsIntro = document.createElement("h3");
   resultsIntro.innerText = `We found ${checkedResults.length} cat(s) for you!`;
   addResult.appendChild(resultsIntro);
 
   checkedResults.forEach((cat) => {
     const div = document.createElement("div");
+    div.classList.add('cat-result')
     addResult.appendChild(div);
 
     const h4 = document.createElement("h4");
@@ -74,21 +77,42 @@ const displayResults = () => {
 
     const description = document.createElement("p");
     description.innerText = `${cat.description}`;
+    description.classList.add('result-description')
     div.appendChild(description);
 
     const temperament = document.createElement("p");
     temperament.innerText = `${cat.temperament}`;
+    temperament.classList.add('result-temperament')
     div.appendChild(temperament);
 
-    if (cat?.image?.url){
+    if (cat?.image?.url) {
       const image = document.createElement("img");
-    image.src = cat.image.url;
-    div.appendChild(image);
+      image.src = cat.image.url;
+      div.appendChild(image);
     } else {
-      const imageLink = ''
+      const imageLink = "";
     }
-    
   });
 };
 
+const cleanResults = () => {
+  document.querySelector(".character-options").style.display = "none";
+  document.querySelector("h1").style.display = "none";
+
+  const newButton = document.createElement('button')
+  newButton.innerText = 'Restart search'
+  newButton.setAttribute('id', 'restart-button')
+  newButton.classList.add('restart-button')
+  document.querySelector('.heading').appendChild(newButton);
+};
+
+const resetSearch = (e) => {
+
+  if (e.target.id === 'restart-button')
+  window.location.reload();
+
+}
+
+button.addEventListener('click', cleanResults)
 button.addEventListener("click", getBreeds);
+document.body.addEventListener('click', resetSearch)
